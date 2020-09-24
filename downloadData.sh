ed729d1423f9b6198e6e9f01b771a614140e0264
deleomike <michaeldeleo31@yahoo.com>
2020-09-24 12:33:00 -0400

    Command for full dataset

--- downloadData.sh
+++ downloadData.sh
@@ -2,5 +2,8 @@
 
 kaggle competitions download -c lyft-motion-prediction-autonomous-vehicles
 
+
 unzip lyft-motion-prediction-autonomous-vehicles.zip -d ./data
+
+kaggle datasets download -d lyft-full-training-set
 

7863c09c9c32ea5f1dc0f6977f1f5ba35c9100f5
Michael DeLeo <michaeldeleo31@yahoo.com>
2020-11-25 14:30:42 -0500

    Quick option

--- oneShot.sh
+++ oneShot.sh
@@ -1,17 +1,14 @@
 #!/usr/bin
 
-git clone https://github.com/deleomike/HelperScripts
-cd HelperScripts
-bash helper.sh -o
+conda update conda
 
-conda init
 conda create -n Lyft python==3.7
 
 conda activate Lyft
 
 conda install -y gxx_linux-64
 
-pip install l5kit==1.0.6 pytorch_pfn_extras
+pip install l5kit==1.0.6 pytorch_pfn_extras horovod
 
 #TODO Prerequisites of horovod
 #HOROVOD_GPU_OPERATIONS=NCCL pip install horovod
